5b1980d747f68a9e2b90c8a284f9088444eeca11
unknown <samsung150973@gmail.com>
2023-04-20 18:58:09 +0100

    Change

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # terraform-vpc
+# Install Terrafile
+# curl -L https://github.com/coretech/terrafile/releases/download/v0.6/terrafile_0.6_Linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin
+
---
+++ main.tf
@@ -0,0 +1,11 @@
+#the code will be sourced through the below repository
+module "vpc" {
+  source = "https://github.com/samsung150973/tf-module-vpc.git=dev"
+}
+
+# The above link will always from main branch. 
+# SOURCE name cannot be parameterised 
+#How to make the code to pick from the required branch?
+# in terraform values cannot be fed by parameters or variables - to make the code dry
+# need a another tool to allow the user to pick the code from the defined source
+# We download the prog named Terrafile fom coretech
