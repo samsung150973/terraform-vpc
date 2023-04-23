1e2591af6b1dba89f676d2de164e7e17df06f9a8
unknown <samsung150973@gmail.com>
2023-04-23 18:44:20 +0100

    Change

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# values for backend will be supplied from the environment specific valiable file 
+# values for backend will be supplied from the environment specific variable files and supply from command line
 terraform {
   backend "s3" {}
 }
